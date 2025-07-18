02d45c2cc9036e70dd8f733c94b5d62051bbd64d
elif_ <elifery06@yandex.com>
2025-07-18 17:03:46 +0300

    navbar giriş yap üe ol butonu eklendi

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -36,15 +36,23 @@ const Navbar = () => {
                 <Link to="/hakkimizda">Hakkımızda</Link>
                 <Link to="/iletisim">İletişim</Link>
                 <Link to="/sikayetlerim">Şikayetlerim</Link>
-            </div>
 
-            <div className="right-section">
                 {!isLoggedIn ? (
                     <div className="auth-links">
                         <Link to="/girisyap" className="auth-btn">Giriş Yap</Link>
                         <Link to="/kayitol" className="auth-btn">Üye Ol</Link>
                     </div>
                 ) : (
+                    <div className="profile-mobile">
+                        <Link to="/profil-ayarlari">Profil</Link>
+                        <Link to="/profil-ayarlari">Ayarlar</Link>
+                        <Link to="/cikis">Çıkış</Link>
+                    </div>
+                )}
+            </div>
+
+            <div className="right-section">
+                {isLoggedIn && (
                     <div className="profile" onClick={toggleDropdown}>
                         <img src={profilePic} alt="Profil" className="profile-pic" />
                         {dropdownOpen && (
